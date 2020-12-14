37721c5bdcc40e4cf9ba26f0052c1d79c8a0ec37
WAW55526 <72281640+WAW55526@users.noreply.github.com>
2020-12-14 14:26:35 +0100

    Create del_1b.py

---
+++ del_1b.py
@@ -0,0 +1,27 @@
+import csv
+
+lats = []
+lons = []
+
+with open('Akvakulturregisteret.csv', newline='', encoding='iso-8859-1') as csvfile:
+    akvareader = csv.reader(csvfile, delimiter=';')
+    for row in akvareader:
+        try:
+            if str(row[12]) == 'Laks':
+                lat = float(row[-2]) # latitude is second last
+                lon = float(row[-1]) # longitude is last
+                
+                lats.append(lat)
+                lons.append(lon)
+
+        except ValueError:
+            continue
+
+try:
+    import matplotlib.pyplot as plt
+    plt.plot(lons,lats,'+', label = 'Laks')
+    plt.legend()
+    plt.show()
+except (ImportError, ModuleNotFoundError) as e:
+    print(f'Import of matplotlib failed: {e}')
+    print(f'We have {len(lats)} latitudes and {len(lons)} longitudes')
